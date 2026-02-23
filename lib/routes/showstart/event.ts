76382d8decccda50f418728030456ce03b907a32
Tony <TonyRL@users.noreply.github.com>
2026-02-23 08:53:08 +0800

    fix(route/showstart): update event and site links

--- lib/routes/showstart/event.ts
+++ lib/routes/showstart/event.ts
@@ -39,7 +39,7 @@ async function handler(ctx: Context): Promise<Data> {
     const tags = [cityName, showName].filter(Boolean).join(' - ');
     return {
         title: `${TITLE} - ${tags}`,
-        link: HOST,
+        link: `${HOST}/event/list?cityCode=${cityCode}${showStyle ? `&showStyle=${showStyle}` : ''}`,
         item: items,
         allowEmpty: true,
     };
